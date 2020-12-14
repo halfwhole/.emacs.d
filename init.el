d4884a61c22726b86231979ce0ff633bf428e156
halfwhole <limziwei2@gmail.com>
2020-12-14 19:38:12 +0800

    Add flycheck and others and reorganise

--- init.el
+++ init.el
@@ -1,4 +1,11 @@
-;; MELPA packages
+;;; init.el --- halfwhole's personal emacs configuration
+
+;;; Code:
+
+;;;;;;;;;;;;;;;;;;;;
+;; MELPA packages ;;
+;;;;;;;;;;;;;;;;;;;;
+
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
@@ -8,16 +15,22 @@
 ;; General configurations ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(desktop-save-mode 1)
 (tool-bar-mode -1)
 (menu-bar-mode -1)
+(scroll-bar-mode -1)
+
 (global-hl-line-mode 1)
 (global-display-line-numbers-mode 1)
 (show-paren-mode 1)
-(scroll-bar-mode -1)
+(desktop-save-mode 1)
+
+(custom-set-variables '(display-time-default-load-average 80)) ; Disable showing the system load average
 (display-time-mode 1)
-(add-to-list 'default-frame-alist '(height . 45))
-(add-to-list 'default-frame-alist '(width . 160))
+
+(add-to-list 'default-frame-alist '(height . 35))
+(add-to-list 'default-frame-alist '(width . 140))
+(custom-set-variables '(split-height-threshold 80)
+		      '(split-width-threshold 160))
 (setq help-window-select t)
 
 ;; Diplays: font, theme, scrollbar, etc.
@@ -29,34 +42,44 @@
 
 ;; Undos and saves
 (global-undo-tree-mode)
-(setq undo-tree-history-directory-alist `(("." . "~/.emacs.d/.undo")))
-(setq backup-directory-alist `(("." . "~/.emacs.d/.saves")))
+(custom-set-variables '(undo-tree-history-directory-alist `(("." . "~/.emacs.d/.undo")))
+		      '(backup-directory-alist `(("." . "~/.emacs.d/.saves"))))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Miscellaneous packages ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(use-package flycheck
+  :ensure t
+  :init (global-flycheck-mode))
 (use-package ivy
   :ensure t
   :config (ivy-mode t))
+(use-package projectile
+  :ensure t
+  :config (projectile-mode t))
+(use-package smex
+  :ensure t
+  :config
+  (global-set-key (kbd "M-x") 'smex)
+  (global-set-key (kbd "M-X") 'smex-major-mode-commands)
+  (global-set-key (kbd "C-c C-c M-x") 'execute-extended-command))
 (use-package which-key
   :ensure t
   :config (which-key-mode t))
 (use-package winner
   :ensure t
   :config (winner-mode t))
-(use-package projectile
-  :ensure t
-  :config (projectile-mode t))
 
 (use-package restart-emacs :ensure t)
 (use-package magit :ensure t)
 (use-package all-the-icons :ensure t)
 (use-package doom-themes :ensure t)
-(use-package doom-modeline :ensure t :init (doom-modeline-mode 1))
+(use-package doom-modeline :ensure t) ; :init (doom-modeline-mode 1))
 (use-package markdown-mode :ensure t)
 (use-package yaml-mode :ensure t)
+(use-package package-lint :ensure t)
 
 
 ;;;;;;;;;;;;;;
@@ -101,6 +124,7 @@
   (setq evil-emacs-state-modes (delq 'package-menu-mode evil-emacs-state-modes))
 
   ;; Leader keybindings
+  ;; For inspiration, see https://github.com/hlissner/doom-emacs/blob/develop/modules/config/default/+evil-bindings.el
   (use-package evil-leader
     :ensure t
     :config
@@ -110,6 +134,7 @@
       "." 'find-file
       "b i" 'ibuffer
       "b k" 'kill-buffer
+      "c x" 'flycheck-list-errors
       "f d" 'dired
       "f p" '(lambda () (interactive) (find-file "~/.emacs.d/init.el"))
       "f s" 'save-buffer
@@ -118,6 +143,8 @@
       "h f" 'describe-function
       "h v" 'describe-variable
       "m p" 'markdown-preview
+      "m m" 'describe-mode
+      "m n" 'describe-minor-mode
       "o t" 'treemacs
       "p a" 'treemacs-add-project-to-workspace
       "p r" 'treemacs-remove-project-from-workspace
@@ -261,10 +288,12 @@
     ("99ea831ca79a916f1bd789de366b639d09811501e8c092c85b2cb7d697777f93" "2f1518e906a8b60fac943d02ad415f1d8b3933a5a7f75e307e6e9a26ef5bf570" "9efb2d10bfb38fe7cd4586afb3e644d082cbcdb7435f3d1e8dd9413cbe5e61fc" "76bfa9318742342233d8b0b42e824130b3a50dcc732866ff8e47366aed69de11" "711efe8b1233f2cf52f338fd7f15ce11c836d0b6240a18fffffc2cbd5bfe61b0" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default)))
  '(package-selected-packages
    (quote
-    (use-package treemacs-evil projectile yaml-mode markdown-mode evil-magit doom-themes evil-surround restart-emacs treemacs evil-escape evil-leader magit ivy-hydra ivy which-key evil))))
+    (flycheck use-package treemacs-evil projectile yaml-mode markdown-mode evil-magit doom-themes evil-surround restart-emacs treemacs evil-escape evil-leader magit ivy-hydra ivy which-key evil))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+;;; init.el ends here
